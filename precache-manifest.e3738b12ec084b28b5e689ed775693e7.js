f27f691e142dfa549da9069ef924c3d83c9256a7
lebohang0824 <lebohang0824@gmail.com>
2020-05-17 06:24:02 +0200

    Initial commit

---
+++ precache-manifest.e3738b12ec084b28b5e689ed775693e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7369f716e6af39d428bdcc32eb7acca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6638667cc36d30cd810f",
+    "url": "/static/css/main.5e55a279.chunk.css"
+  },
+  {
+    "revision": "faa0691c67e6a9754053",
+    "url": "/static/js/2.817186b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.817186b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6638667cc36d30cd810f",
+    "url": "/static/js/main.eb2e4a71.chunk.js"
+  },
+  {
+    "revision": "c6a5e717b3706788d5b3",
+    "url": "/static/js/runtime-main.3d60ae38.js"
+  },
+  {
+    "revision": "bc0f8831fbbf66a4883e12e3d63a896e",
+    "url": "/static/media/bg.bc0f8831.jpg"
+  }
+]);
